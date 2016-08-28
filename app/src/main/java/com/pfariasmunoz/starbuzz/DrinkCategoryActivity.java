a6be892114a775a996f8a5e2bbb7c2c6cd2f5735
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-28 18:10:18 -0300

    Implements onListItemClick as listener for the ListView

--- app/src/main/java/com/pfariasmunoz/starbuzz/DrinkCategoryActivity.java
+++ app/src/main/java/com/pfariasmunoz/starbuzz/DrinkCategoryActivity.java
@@ -4,6 +4,8 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.view.View;
+import android.content.Intent;
 
 public class DrinkCategoryActivity extends ListActivity {
 
@@ -17,5 +19,12 @@ public class DrinkCategoryActivity extends ListActivity {
                 android.R.layout.simple_expandable_list_item_1,
                 Drink.drinks);
         listDrinks.setAdapter(listAdapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+        intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
+        startActivity(intent);
     }
 }
