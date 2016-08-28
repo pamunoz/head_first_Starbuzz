608c1fd8cb51251c50ab4b9bd6d5ef79eb529e92
Pablo Munoz <pfariasmunoz@gmail.com>
2016-08-28 16:05:20 -0300

    Add a lisstener to the Listview

---
+++ app/src/main/java/com/pfariasmunoz/starbuzz/DrinkCategoryActivity.java
@@ -0,0 +1,13 @@
+package com.pfariasmunoz.starbuzz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrinkCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_category);
+    }
+}
--- app/src/main/java/com/pfariasmunoz/starbuzz/TopLevelActivity.java
+++ app/src/main/java/com/pfariasmunoz/starbuzz/TopLevelActivity.java
@@ -1,7 +1,10 @@
 package com.pfariasmunoz.starbuzz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 public class TopLevelActivity extends AppCompatActivity {
 
@@ -10,4 +13,16 @@ public class TopLevelActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
     }
+
+    AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> listView,
+                                View itemView,
+                                int position,
+                                long id) {
+            if (position == 0) {
+                Intent intent = new Intent(TopLevelActivity.this, DrinkCategoryActivity.class);
+                startActivity(intent);
+            }
+        }
+    };
 }
